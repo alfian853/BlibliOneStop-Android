1c3bfc12cdf75ff9cda4705f7e4b84fe2e26e3ed
alfian <alfian853@gmail.com>
2019-11-12 20:40:42 +0700

    resolve code review

--- library/src/main/java/com/gdn/android/onestop/library/fragment/AudioCatalogFragment.kt
+++ library/src/main/java/com/gdn/android/onestop/library/fragment/AudioCatalogFragment.kt
@@ -8,13 +8,6 @@ import com.gdn.android.onestop.base.BaseFragment
 import com.gdn.android.onestop.library.databinding.LayoutPageAudioBinding
 
 class AudioCatalogFragment : BaseFragment<LayoutPageAudioBinding>() {
-  override fun doFragmentInjection() {
-
-  }
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-  }
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
       savedInstanceState: Bundle?): View? {
