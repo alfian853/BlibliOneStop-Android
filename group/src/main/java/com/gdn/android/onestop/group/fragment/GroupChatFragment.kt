08b0f62401ff4b72eb56bdaf1381e3a97a5ef8f5
alfian <alfian853@gmail.com>
2020-01-07 20:53:08 +0700

    resolving PR issue

--- group/src/main/java/com/gdn/android/onestop/group/fragment/GroupChatFragment.kt
+++ group/src/main/java/com/gdn/android/onestop/group/fragment/GroupChatFragment.kt
@@ -108,7 +108,7 @@ class GroupChatFragment : BaseFragment<FragmentChatRoomBinding>(){
     override fun onItemClick(item: String, position: Int) {
       val fragment: DialogFragment = Navigator.getFragment(Navigator.Destination.PROFILE_DIALOG_FRAGMENT) as DialogFragment
       val bundle = Bundle()
-      bundle.putString("username",item)
+      bundle.putString(Navigator.Argument.PROFILE_USERNAME.key, item)
       fragment.arguments = bundle
       fragment.show(fragmentManager!!, "profile fragment")
 
